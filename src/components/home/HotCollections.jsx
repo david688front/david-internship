b754c4e32a3504eb221acb33f8ddaf95c5034aa4
David <d888eightsix@icloud.com>
2023-10-17 16:51:28 +0700

    loading fix

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState , Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import Skeleton from "../UI/Skeleton";
 
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
@@ -9,10 +10,11 @@ import 'owl.carousel/dist/assets/owl.theme.default.css';
 const HotCollections = () => {
 
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState([]);
+  const [isLoading, setIsLoading] = useState([false]);
 
   useEffect(() => {
     // Fetch data from the URL
+    setIsLoading(true);
     fetch('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections')
       .then((response) => {
         if (!response.ok) {
@@ -25,12 +27,13 @@ const HotCollections = () => {
       .then((data) => {
         // Assuming the response is an array, you can set it to the state
         setData(data);
-        setLoading(false);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
   }, []);
+    
 
   const options = {
     loop: true ,
@@ -64,25 +67,63 @@ const HotCollections = () => {
           </div>
 
         <OwlCarousel className='owl-theme' {...options}>
-            {data.map((item, index) => (
-                <div key={index}>
+            {isLoading ? data.map((item) => (
+               <div key={item.id}>
+               <div className="nft_coll">
+               <div className="nft_wrap">
+               <Link to={`/item-details/${item.nftId}`}>
+                   <img src={item.nftImage} className="lazy img-fluid" alt="" />
+                 </Link>
+               </div>
+               <div className="nft_coll_pp">
+                 <Link to={`/author/${item.authorId}`}>
+                   <img className="lazy pp-coll" src={item.authorImage} alt="" />
+                 </Link>
+                 <i className="fa fa-check"></i>
+               </div>
+               <div className="nft_coll_info">
+                 <Link to="/explore">
+                   <h4>{item.title}</h4>
+                 </Link>
+                 <span>ERC-{item.code}</span>
+               </div>
+             </div>
+             </div>
+            ))
+                  : data.map((item) => (
+
+                <div key={item.id}>
                 <div className="nft_coll">
                 <div className="nft_wrap">
-                <Link to={`/item-details/${item.nftId}`}>
-                    <img src={item.nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
+                <Skeleton
+                        width="100%"
+                        height="200px"
+                        borderRadius="8px"
+                      />
+
                 </div>
                 <div className="nft_coll_pp">
-                  <Link to={`/author/${item.authorId}`}>
-                    <img className="lazy pp-coll" src={item.authorImage} alt="" />
-                  </Link>
+                        <Skeleton
+                          width="40px"
+                          height="40px"
+                          borderRadius="50%"
+                        />
                   <i className="fa fa-check"></i>
+
                 </div>
                 <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>{item.title}</h4>
-                  </Link>
-                  <span>ERC-{item.code}</span>
+                <Skeleton
+                          width="70%"
+                          height="24px"
+                          borderRadius="4px"
+                        />
+                        <Skeleton
+                          width="40%"
+                          height="18px"
+                          borderRadius="4px"
+                        />
+                  
+
                 </div>
               </div>
               </div>
